4be87876f1c5ee5fcd1a98734e0136affe223148
KibbeWater <35224538+KibbeWater@users.noreply.github.com>
2024-11-22 02:02:09 +0100

    feat: init commit

---
+++ src/main/java/com/kibbewater/noteblock_backport/NoteblockBackport.java
@@ -0,0 +1,31 @@
+package com.kibbewater.noteblock_backport;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = NoteblockBackport.MODID, name = NoteblockBackport.NAME, version = NoteblockBackport.VERSION)
+public class NoteblockBackport
+{
+    public static final String MODID = "noteblock_backport";
+    public static final String NAME = "Noteblock Backport";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
